9cf7877d3581a81e6047fb48fecaa6359f5d14a0
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-05-23 18:15:59 +0800

    feat: 学习文件上传操作

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayHelloName(w http.ResponseWriter, r *http.Request) {
@@ -47,10 +52,56 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		// 生成时间戳
+		currentTime := time.Now().Unix()
+		// 新建md5
+		h := md5.New()
+		// 将时间戳转为10进制字符串写入md5
+		_, err := io.WriteString(h, strconv.FormatInt(currentTime, 10))
+		if err != nil {
+			log.Fatalln("io.WriteString err:", err)
+		}
+		// 将md5转为16进制字符串
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		// 解析文件
+		t, _ := template.ParseFiles("upload.gtpl")
+		// 写入token到文件中
+		err = t.Execute(w, token)
+		if err != nil {
+			log.Fatalln("t.Execute err:", err)
+		}
+	} else {
+		err := r.ParseMultipartForm(32 << 20)
+		if err != nil {
+			return
+		}
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			log.Fatalln("r.FormFile err:", err)
+		}
+		defer file.Close()
+		_, err = fmt.Fprintf(w, "%v", handler.Header)
+		if err != nil {
+			return
+		}
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatalln("os.OpenFile err:", err)
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
 func main() {
 	// 拦截器的方法
 	http.HandleFunc("/", sayHelloName)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
 	// 监听端口
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
