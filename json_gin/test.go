1d3041733b3ea37768cdfd5591ae914d4e639bba
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-05-28 15:52:27 +0800

    feat: 添加 XML 和 YAML 渲染功能到用户接口

--- json_gin/test.go
+++ json_gin/test.go
@@ -18,11 +18,28 @@ func main() {
 	r.GET("/user", func(ctx *gin.Context) {
 		ctx.JSON(200, users)
 	})
+	// gin.H是map[string]interface的缩写
 	r.GET("/userT", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "Hello, World!",
 			"code":    200,
 		})
 	})
+	// xml渲染
+	r.GET("/userXml", func(ctx *gin.Context) {
+		ctx.XML(200, users)
+	})
+	// gin.H是map[string]interface的缩写
+	r.GET("/userXmlT", func(ctx *gin.Context) {
+		ctx.XML(200, gin.H{
+			"message": "Hello, World!",
+			"code":    200,
+		})
+	})
+	// yaml渲染
+	r.GET("/userYaml", func(ctx *gin.Context) {
+		ctx.YAML(200, users)
+	})
+
 	r.Run(":8080")
 }
